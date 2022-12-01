d9fae08ca2011a554f22705871a406fd8216883f
LorenzoOgnibene <lorenzo.ognibene@gmail.com>
2022-12-01 16:16:55 +0100

    First view of random bomb

--- js/javaScript.js
+++ js/javaScript.js
@@ -11,17 +11,39 @@ function createSquareElement(content, classes, w, h){
     return square;
 }
 
+function getRandomNumber (numMin, numMax){
+    const randomNumber = Math.floor(Math.random() * (numMin - numMax + 1) + numMin);
+    return randomNumber;
+}
+
+//legal note it's olny a virtual game
+function randomBombGenerator(array, min, max){
+    let isValid = false;
+    let randomBomb;
+    
+    while (isValid === false){
+        randomBomb = getRandomNumber(min, max);
+        
+        if(!array.includes(randomBomb)){
+         isValid = true;
+        }
+    }
+
+     return randomBomb;
+}
 
 const containerElement = document.querySelector('div.big-square');
 const newSquare = createSquareElement();
 const playButton = document.querySelector('button.btn');
 
-
 playButton.addEventListener('click', function(){
     let width = 10;
     let height = 10;
     const gridSelection = document.getElementById('grid-value').value;
+    const bombNum =[];
+    const gridNum =[];
     containerElement.innerHTML = ' ';
+    console.log(bombNum)
     if(gridSelection == 65){
         width = 8;
         height = 8;
@@ -31,5 +53,13 @@ playButton.addEventListener('click', function(){
     }
     for(let i = 1; i < gridSelection; i++){
         containerElement.appendChild(createSquareElement(i, 'square', width, height));
+        gridNum.push(i);
     }
+    console.log(gridNum);
+    
+    for(let i = 0; i < 16; i++){
+        bombNum.push(randomBombGenerator(bombNum, 0, gridNum.length))
+    }
+    console.log(bombNum)
 })
+
